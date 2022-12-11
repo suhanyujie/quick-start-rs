77a1585e78e9505e936fd76d73bf3401a2648441
suhanyujie <suhanyu@bjx.cloud>
2022-12-11 18:23:37 +0800

    update code for use crate

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ mod prelude;
 mod utils;
 
 fn main() {
+    // test call. You can delete it
     utils::lib1();
+    // test call. You can delete it
     println!("{:?}", get_files_by_dir("./".to_string()));
 }
 
 /// read file list in some dir
+/// test function. You can delete it
 fn get_files_by_dir(dir: String) -> Result<Vec<String>> {
     let mut list = vec![];
     for entry in fs::read_dir(dir)?.filter_map(|item| item.ok()) {
--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,8 @@
-use crate::errors::Error;
+pub use crate::errors::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 // Your wrapper type
 pub struct W<T>(pub T);
+
+pub use std::format as f;
